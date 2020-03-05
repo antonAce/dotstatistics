698bbae983fe7ddb28f55834efdbfea8c1ce1085
Anton Kozyriev <sonara4mus@gmail.com>
2020-03-05 23:45:36 +0200

    UI: Replacing table cells with components

--- ui/src/app/statistics-table/statistics-cell/statistics-cell.component.ts
+++ ui/src/app/statistics-table/statistics-cell/statistics-cell.component.ts
@@ -1,11 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-statistics-cell',
+  selector: 'statistics-cell',
   templateUrl: './statistics-cell.component.html',
   styleUrls: ['./statistics-cell.component.scss']
 })
 export class StatisticsCellComponent implements OnInit {
+  @Input() isReadonly: boolean;
+
+  @Input()
+  get value() {
+    return this._value;
+  }
+
+  @Output() valueChange = new EventEmitter();
+
+  set value(val) {
+    this._value = val;
+    this.valueChange.emit(this._value);
+  }
+
+  private _value: number;
 
   constructor() { }
 
