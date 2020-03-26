da91e1484c76323ad073b088754a263221e1c864
Anton Kozyriev <sonara4mus@gmail.com>
2020-03-26 17:44:53 +0200

    API: Analytics controller

---
+++ src/Regression.API/Controllers/AnalysisController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using Regression.API.Filters;
+using Regression.API.Models;
+using Regression.Calculation.Contracts;
+using Regression.Calculation.DTO;
+
+using Regression.BL.DTO;
+using Regression.BL.Interfaces;
+
+namespace Regression.API.Controllers
+{
+    [ApiController]
+    [ProcessingExceptionFilter]
+    [Route("api/[controller]")]
+    public class AnalysisController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IDatasetService _datasetService;
+        private readonly IRegressionService _regressionService;
+
+        public AnalysisController(ILogger<AnalysisController> logger,
+                                  IDatasetService datasetService,
+                                  IRegressionService regressionService)
+        {
+            _logger = logger;
+            _datasetService = datasetService;
+            _regressionService = regressionService;
+        }
+        
+        [HttpGet("{id}/equation")]
+        public async Task<IActionResult> CalculateEquation(Guid id, [FromQuery] int? digits)
+        {
+            _logger.LogInformation($"[{DateTime.Now}] Calculate equation for dataset #{id}");
+            var dataset = await _datasetService.GetDatasetById(id);
+            
+            ICollection<double> poly = _regressionService.CalculateRegression(
+                dataset.Records.Select(row => new Record
+                {
+                    Inputs = row.Inputs,
+                    Output = row.Output
+                }).ToArray());
+
+            if (digits.HasValue)
+                poly = poly.Select(number => Math.Round(number, digits.Value)).ToArray();
+            
+            return Ok(new PolynomialModel { Koeficients = poly });
+        }
+    }
+}
