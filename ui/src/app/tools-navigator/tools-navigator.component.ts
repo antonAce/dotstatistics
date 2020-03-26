d10a1870c41a2945cfbe8e3c00b1770b3043c949
Anton Kozyriev <sonara4mus@gmail.com>
2020-03-26 19:51:11 +0200

    UI: Fix link overriding issue

--- ui/src/app/tools-navigator/tools-navigator.component.ts
+++ ui/src/app/tools-navigator/tools-navigator.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'tools-navigator',
   templateUrl: './tools-navigator.component.html',
   styleUrls: ['./tools-navigator.component.scss']
 })
 export class ToolsNavigatorComponent{
-  modelId: string;
+  modelId: Observable<string>;
 
   constructor(private activateRoute: ActivatedRoute) {
-    this.modelId = this.activateRoute.snapshot.params['id'];
+    this.modelId = this.activateRoute.params.pipe(map(param => param['id']));
   }
 }
