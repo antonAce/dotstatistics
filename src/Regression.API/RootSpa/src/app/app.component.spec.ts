465306e6b287d1ade8614b2064bb5200d47244a3
Anton Kozyriev <sonara4mus@gmail.com>
2020-09-26 02:23:54 +0300

    feat: set root spa project name

--- src/Regression.API/RootSpa/src/app/app.component.spec.ts
+++ src/Regression.API/RootSpa/src/app/app.component.spec.ts
@@ -1,35 +1,13 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  let component;
+
+  beforeEach(() => {
+    component = new AppComponent();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'regression-ui'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('regression-ui');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('regression-ui app is running!');
+    expect(component).toBeTruthy();
   });
 });
