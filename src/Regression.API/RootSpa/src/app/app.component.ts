6167619880e4c7645fc1514ea6d47009e51cfe63
Anton Kozyriev <sonara4mus@gmail.com>
2020-09-29 01:26:41 +0300

    header component mockup

--- src/Regression.API/RootSpa/src/app/app.component.ts
+++ src/Regression.API/RootSpa/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'root',
@@ -6,4 +7,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  items: MenuItem[] = [
+    {
+      label: 'Workspace',
+      items: [
+        {
+          label: 'New',
+          icon: 'pi pi-fw pi-plus'
+        },
+        {
+          label: 'Open',
+          icon: 'pi pi-fw pi-folder-open'
+        },
+        {
+          label: 'Save',
+          icon: 'pi pi-fw pi-save',
+          items: [
+            {
+              label: 'Save as'
+            }
+          ]
+        },
+        {
+          label: 'Import',
+          icon: 'pi pi-fw pi-cloud-upload'
+        },
+        {
+          label: 'Export',
+          icon: 'pi pi-fw pi-cloud-download'
+        },
+        {
+          label: 'Delete',
+          icon: 'pi pi-fw pi-trash'
+        },
+        {
+          label: 'Refresh',
+          icon: 'pi pi-fw pi-refresh'
+        }
+      ]
+    },
+    {
+      label: 'Edit',
+      items: [
+        {
+          label: 'Cut'
+        },
+        {
+          label: 'Copy'
+        },
+        {
+          label: 'Paste'
+        }
+      ]
+    },
+    {
+      label: 'Analysis',
+      items: [
+        {
+          label: 'Models'
+        }
+      ]
+    }
+  ];
 }
--- src/Regression.API/RootSpa/src/app/app.module.ts
+++ src/Regression.API/RootSpa/src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MenubarModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
