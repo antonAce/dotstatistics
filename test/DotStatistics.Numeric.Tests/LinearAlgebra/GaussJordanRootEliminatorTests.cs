3e604cff47890d29941e8cd1404edee8c70d85d7
Anton Kozyriev <sonara4mus@gmail.com>
2020-10-14 00:19:56 +0300

    feat(Numeric.Tests): gauss-jordan negative test cases

--- test/DotStatistics.Numeric.Tests/LinearAlgebra/GaussJordanRootEliminatorTests.cs
+++ test/DotStatistics.Numeric.Tests/LinearAlgebra/GaussJordanRootEliminatorTests.cs
@@ -15,8 +15,14 @@ namespace DotStatistics.Numeric.Tests.LinearAlgebra
         public GaussJordanRootEliminatorTests() =>
             _eliminator = new GaussJordanRootEliminator();
 
-        public static IEnumerable<object[]> GaussJordanRootEliminatorTestCases()
+        public static IEnumerable<object[]> GaussJordanRootEliminatorPositiveTestCases()
         {
+            yield return new object[]
+            {
+                new Matrix(new double[,] {{2, 1}, {1, 2}}),
+                new Matrix(new double[,] {{1}, {1.5}}),
+                new Matrix(new double[,] {{0.16666667}, {0.66666667}})
+            };
             yield return new object[]
             {
                 new Matrix(new double[,] {{2, 1, -1}, {-3, -1, 2}, {-2, 1, 2}}),
@@ -36,13 +42,49 @@ namespace DotStatistics.Numeric.Tests.LinearAlgebra
                 new Matrix(new[,] {{-1.0}, {-2.0}, {-1.0}, {-2.0}})
             };
         }
+        
+        public static IEnumerable<object[]> GaussJordanRootEliminatorNegativeTestCases()
+        {
+            yield return new object[]
+            {
+                new Matrix(new double[,] {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, {0, 0, 1}}),
+                new Matrix(new double[,] {{1}, {2}, {3}}),
+                "The coefficient matrix should be squared."
+            };
+            yield return new object[]
+            {
+                new Matrix(new double[,] {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}),
+                new Matrix(new double[,] {{1, 0}, {2, 0}, {3, 1}}),
+                "The width of the outputs vector should equal to 1."
+            };
+            yield return new object[]
+            {
+                new Matrix(new[,] {{1.0, 2.0, -3.0, -1.0}, {0.0, -3.0, 2.0, 6.0}, {0.0, 5.0, -6.0, -2.0}, {0.0, -1.0, 8.0, 1.0}}),
+                new Matrix(new[,] {{0.0}, {0.0}, {0.0}}),
+                "The height of the coefficient matrix and outputs vector should match."
+            };
+            yield return new object[]
+            {
+                new Matrix(new double[,] {{1, 1, 1}, {2, 2, 3}, {4, 4, 6}}),
+                new Matrix(new double[,] {{1}, {2}, {3}}),
+                "Coefficient matrix has linear dependent rows."
+            };
+        }
 
         [Theory]
         [MemberData(
-            nameof(GaussJordanRootEliminatorTestCases),
+            nameof(GaussJordanRootEliminatorPositiveTestCases),
             MemberType = typeof(GaussJordanRootEliminatorTests))]
-        public void AddOperation_PositiveTestCases(IMatrix coefficients, IMatrix outputs, IMatrix roots) =>
+        public void GaussJordan_PositiveTestCases(IMatrix coefficients, IMatrix outputs, IMatrix roots) =>
             ColumnVectorApproximatelyEqual(_eliminator.Eliminate(coefficients, outputs), roots);
+        
+        [Theory]
+        [MemberData(
+            nameof(GaussJordanRootEliminatorNegativeTestCases),
+            MemberType = typeof(GaussJordanRootEliminatorTests))]
+        public void GaussJordan_NegativeTestCases(IMatrix coefficients, IMatrix outputs, string message) =>
+            new Action(() => _eliminator.Eliminate(coefficients, outputs)).Should()
+                .ThrowExactly<InvalidOperationException>().WithMessage(message);
 
         private void ColumnVectorApproximatelyEqual(IMatrix expected, IMatrix actual)
         {
